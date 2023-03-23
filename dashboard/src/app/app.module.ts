a727f625dcf448e79f9676ebb18e937aa9560c0c
c.yenal <c.yenal@it-excelsus.de>
2023-03-23 15:48:38 +0100

    dashboard

---
+++ dashboard/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { MainComponent } from './main/main.component';
+import { TopWidgetsComponent } from './top-widgets/top-widgets.component';
+import { SalesByMonthComponent } from './sales-by-month/sales-by-month.component';
+import { SalesByCategoryComponent } from './sales-by-category/sales-by-category.component';
+import { LastFewTransactionsComponent } from './last-few-transactions/last-few-transactions.component';
+import { TopThreeProductsComponent } from './top-three-products/top-three-products.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SideNavComponent,
+    MainComponent,
+    TopWidgetsComponent,
+    SalesByMonthComponent,
+    SalesByCategoryComponent,
+    LastFewTransactionsComponent,
+    TopThreeProductsComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {
+  faCoffee = faCoffee;
+}
---
+++ dashboard/src/app/side-nav/side-nav.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+@Component({
+  selector: 'app-side-nav',
+  templateUrl: './side-nav.component.html',
+  styleUrls: ['./side-nav.component.scss'],
+})
+export class SideNavComponent implements OnInit {
+  faDashboard = faDashboard;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
