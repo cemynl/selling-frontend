485e10eecffc08b2eb77ee344231386f4c09ddce
cemynl <cem.ynl@icloud.com>
2023-08-24 22:22:35 +0200

    added new features

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ChartModule } from 'angular-highcharts';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { SideNavComponent } from './side-nav/side-nav.component';
-import { MainComponent } from './main/main.component';
-import { TopWidgetsComponent } from './top-widgets/top-widgets.component';
-import { SalesByMonthComponent } from './sales-by-month/sales-by-month.component';
-import { SalesByCategoryComponent } from './sales-by-category/sales-by-category.component';
 import { LastFewTransactionsComponent } from './last-few-transactions/last-few-transactions.component';
+import { MainComponent } from './main/main.component';
+import { SalesByCategoryComponent } from './sales-by-category/sales-by-category.component';
+import { SalesByMonthComponent } from './sales-by-month/sales-by-month.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 import { TopThreeProductsComponent } from './top-three-products/top-three-products.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
+import { TopWidgetsComponent } from './top-widgets/top-widgets.component';
 
 @NgModule({
   declarations: [
@@ -24,11 +23,14 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     SalesByMonthComponent,
     SalesByCategoryComponent,
     LastFewTransactionsComponent,
-    TopThreeProductsComponent,
+    TopThreeProductsComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+  imports: [
+    BrowserModule,
+    FontAwesomeModule,
+    ChartModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
